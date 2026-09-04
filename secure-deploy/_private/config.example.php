97356b99e419f58d18118c1232638c437459ca0d
alyagorokhoff <alya.gorokhoff@gmail.com>
2026-09-04 20:04:47 +0300

    Add protected access module: _private/config.example.php

---
+++ secure-deploy/_private/config.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'app_secret' => 'CHANGE_TO_A_RANDOM_64_CHARACTER_SECRET',
+    'prodamus_secret' => 'PASTE_PRODAMUS_SECRET_HERE',
+    'mail_from' => 'access@archangelsguide.ru',
+    'mail_from_name' => 'ALYA · Живая колода',
+    'premium_markers' => ['PREMIUM', 'ПРЕМИУМ', 'ЖИВАЯ КОЛОДА'],
+    'premium_product_ids' => [],
+    'support_email' => 'support@archangelsguide.ru',
+];
+
